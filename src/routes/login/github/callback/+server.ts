42a79b375afa987955bf43a62b62d64ab5e3f797
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-07-20 18:38:53 +0200

    Add error text

--- src/routes/login/github/callback/+server.ts
+++ src/routes/login/github/callback/+server.ts
@@ -71,13 +71,9 @@ export async function GET(event: RequestEvent): Promise<Response> {
 		// the specific error message depends on the provider
 		if (e instanceof OAuth2RequestError) {
 			// invalid code
-			return new Response(null, {
-				status: 400
-			});
+			return new Response(null, { status: 400 });
 		}
-		return new Response(null, {
-			status: 500
-		});
+		return new Response('Unspecified error', { status: 500 });
 	}
 }
 
