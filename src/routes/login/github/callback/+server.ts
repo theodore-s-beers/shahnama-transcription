361f09a0dd774a6758127472613fe7835c0b00d0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-21 12:56:00 -0500

    Try replacing non-ASCII characters

--- src/routes/login/github/callback/+server.ts
+++ src/routes/login/github/callback/+server.ts
@@ -21,7 +21,7 @@ export async function GET({ cookies, platform, url }) {
 		const githubUserResponse = await fetch("https://api.github.com/user", {
 			headers: {
 				Authorization: `Bearer ${tokens.accessToken}`,
-				"User-Agent": "Shāhnāma Transcription Alpha",
+				"User-Agent": "Shahnama Transcription Alpha",
 			},
 		});
 
