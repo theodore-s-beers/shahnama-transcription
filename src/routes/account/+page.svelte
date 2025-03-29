aed2f47f743c07786320448caeb0b423418d41e6
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-03-29 11:44:33 -0400

    Work on account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import type { PageProps } from "./$types";
 
 	let { data }: PageProps = $props();
 	const shortName = data.shortName;
+
+	interface PageNumber {
+		vol: string; // Number (which will be parsed) or "N/A"
+		pg: string; // Number (which will be parsed) or "N/A"
+	}
+
+	let myPages: PageNumber[] = $state([]);
+
+	onMount(() => {
+		if (myPages.length === 0) {
+			myPages.push({
+				vol: "N/A",
+				pg: "N/A",
+			});
+		}
+	});
 </script>
 
 <div class="mx-auto max-w-7xl p-4 text-lg">
+	<div class="mb-3 flex justify-end">
+		<div class="mr-8"><a href="/" class="text-blue-800 hover:underline">Home</a></div>
+		<div><a href="/logout" class="text-blue-800 hover:underline">Log out</a>?</div>
+	</div>
+
 	<div class="mb-6 text-4xl">User Account: {shortName}</div>
 
-	<div>Want to <a href="/logout" class="text-blue-800 hover:underline">log out</a>?</div>
+	<div>Pages you have submitted:</div>
+
+	<ul class="list-disc">
+		{#each myPages as { vol, pg } (pg)}
+			<li>vol. {vol}, pg. {pg}</li>
+		{/each}
+	</ul>
 </div>
