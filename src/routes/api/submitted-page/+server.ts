ea02d73687954cb29a683b2c4abe2b93907c33fe
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-03-29 14:35:54 -0400

    Add another endpoint

---
+++ src/routes/api/submitted-page/+server.ts
@@ -0,0 +1,78 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { maxPages, type Line } from "$lib/utils";
+
+export const GET: RequestHandler = async ({ locals, platform, request }) => {
+	// Same origin only (if origin is provided)
+	const origin = request.headers.get("origin");
+	if (origin && origin !== "https://shahnama-transcription.pages.dev") {
+		return new Response("Invalid origin", { status: 403 });
+	}
+
+	// Committers only
+	const editor = locals.user?.shortName;
+	if (!editor) return new Response("User not authorized", { status: 403 });
+
+	// Volume and page required as query params
+	const params = new URL(request.url).searchParams;
+	const [volParam, pgParam] = [params.get("vol"), params.get("pg")];
+	if (!volParam || !pgParam) return new Response("Missing volume or page", { status: 400 });
+
+	// Volume and page must be valid
+	const vol = parseInt(volParam);
+	if (vol < 1 || vol > 8) return new Response("Invalid volume", { status: 400 });
+	const pg = parseInt(pgParam);
+	if (pg < 3 || pg > maxPages[vol]) return new Response("Invalid page", { status: 400 });
+
+	const db = platform!.env.DB;
+
+	const sql = `
+    SELECT
+      number_within_page,
+      heading,
+      heading_text,
+      number_listed,
+      hemistich_one_text,
+      hemistich_one_notes,
+      hemistich_two_text,
+      hemistich_two_notes
+    FROM line
+    WHERE volume_number = $1 AND page_number = $2 AND editor = $3
+    ORDER BY number_within_page;
+  `;
+
+	const stmt = db.prepare(sql).bind(vol, pg, editor);
+	const { results } = await stmt.all<RawLine>();
+
+	if (results.length === 0) return new Response("No transcription found", { status: 404 });
+
+	const lines: Line[] = results.map((row) => ({
+		heading: !!row.heading,
+		headingText: row.heading_text ?? undefined,
+		numberWithinPage: row.number_within_page,
+		numberListed: row.number_listed ?? undefined,
+		hemistichOne: {
+			text: row.hemistich_one_text ?? undefined,
+			hasNotes: !!row.hemistich_one_notes,
+		},
+		hemistichTwo: {
+			text: row.hemistich_two_text ?? undefined,
+			hasNotes: !!row.hemistich_two_notes,
+		},
+	}));
+
+	return new Response(JSON.stringify(lines), {
+		status: 200,
+		headers: { "content-type": "application/json" },
+	});
+};
+
+interface RawLine {
+	number_within_page: number;
+	heading: number;
+	heading_text: string | null;
+	number_listed: number | null;
+	hemistich_one_text: string | null;
+	hemistich_one_notes: number | null;
+	hemistich_two_text: string | null;
+	hemistich_two_notes: number | null;
+}
