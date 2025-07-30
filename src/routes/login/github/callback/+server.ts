d89a79ddaa78105242c2a6c8e1e0737142f84d49
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-07-30 12:52:18 -0400

    Troubleshoot cookie setting

--- src/routes/login/github/callback/+server.ts
+++ src/routes/login/github/callback/+server.ts
@@ -1,13 +1,7 @@
 import { OAuth2RequestError } from "arctic";
 import type { RequestHandler } from "./$types";
 import { v7 as uuidv7 } from "uuid";
-import {
-	type UserRow,
-	SESSION_COOKIE_NAME,
-	SESSION_EXPIRES_SECONDS,
-	createSession,
-	github,
-} from "$lib/server/auth";
+import { type UserRow, SESSION_COOKIE_NAME, createSession, github } from "$lib/server/auth";
 
 export const GET: RequestHandler = async ({ cookies, platform, url }) => {
 	const code = url.searchParams.get("code");
@@ -47,13 +41,8 @@ export const GET: RequestHandler = async ({ cookies, platform, url }) => {
 
 		const sessionWithToken = await createSession(platform!.env.DB, userId);
 
-		cookies.set(SESSION_COOKIE_NAME, sessionWithToken.token, {
-			path: "/",
-			secure: import.meta.env.PROD,
-			httpOnly: true,
-			maxAge: SESSION_EXPIRES_SECONDS,
-			sameSite: "lax",
-		});
+		// Minimal options, for debugging
+		cookies.set(SESSION_COOKIE_NAME, sessionWithToken.token, { path: "/" });
 
 		return new Response(null, {
 			status: 302,
