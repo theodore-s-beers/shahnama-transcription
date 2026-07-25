0692869e02dd007bca0fb4366e52f06f58e4b514
Theo Beers <theo.beers@fu-berlin.de>
2026-07-25 10:56:08 -0400

    update transcribed lines query

--- src/routes/api/transcribed-lines/+server.ts
+++ src/routes/api/transcribed-lines/+server.ts
@@ -54,18 +54,29 @@ export const GET: RequestHandler = async ({ platform, request }) => {
 
 	const startVol = parseInt(startVolParam);
 	const startPg = parseInt(startPgParam);
-	if (!validSelection(startVol, startPg))
+	if (!validSelection(startVol, startPg)) {
 		return new Response("Invalid start volume or page", { status: 400 });
+	}
 
 	const endVol = parseInt(endVolParam);
 	const endPg = parseInt(endPgParam);
-	if (!validSelection(endVol, endPg))
+	if (!validSelection(endVol, endPg)) {
 		return new Response("Invalid end volume or page", { status: 400 });
+	}
 
 	const startLine = parseInt(startLineParam);
 	const endLine = parseInt(endLineParam);
-	if (startLine < 1 || startLine > 25 || endLine < 1 || endLine > 25)
+	if (startLine < 1 || startLine > 25 || endLine < 1 || endLine > 25) {
 		return new Response("Invalid start or end line number", { status: 400 });
+	}
+
+	if (
+		startVol > endVol ||
+		(startVol === endVol && startPg > endPg) ||
+		(startVol === endVol && startPg === endPg && startLine > endLine)
+	) {
+		return new Response("Start must precede end", { status: 400 });
+	}
 
 	// If we made it this far, we need the DB
 	const db = platform!.env.DB;
@@ -83,13 +94,15 @@ export const GET: RequestHandler = async ({ platform, request }) => {
 		SELECT *
 		FROM line_simplified
 		WHERE editor = ?
-			AND volume_number BETWEEN ? AND ?
-			AND page_number BETWEEN ? AND ?
+			AND (volume_number, page_number, number_within_page) >= (?, ?, ?)
+			AND (volume_number, page_number, number_within_page) <= (?, ?, ?)
 		ORDER BY volume_number, page_number, number_within_page
 		LIMIT 1152;
 	`;
 
-	const lineStmt = db.prepare(lineSql).bind(editor, startVol, endVol, startPg, endPg);
+	const lineStmt = db
+		.prepare(lineSql)
+		.bind(editor, startVol, startPg, startLine, endVol, endPg, endLine);
 	const { results } = await lineStmt.all<RawLine>();
 
 	if (results.length === 0) return new Response("No relevant lines found", { status: 404 });
@@ -107,30 +120,10 @@ export const GET: RequestHandler = async ({ platform, request }) => {
 		hemistichTwo: row.hemistich_two_text ?? undefined,
 	}));
 
-	// We grabbed whole pages' worth of lines and may need to remove
-	// leading or trailing lines from the first or last page, respectively.
-	const linesFiltered = lines.filter((line) => {
-		if (
-			line.volumeNumber === startVol &&
-			line.pageNumber === startPg &&
-			line.numberWithinPage < startLine
-		)
-			return false;
-
-		if (
-			line.volumeNumber === endVol &&
-			line.pageNumber === endPg &&
-			line.numberWithinPage > endLine
-		)
-			return false;
-
-		return true;
-	});
-
 	const headers = new Headers({ "content-type": "application/json" });
 	if (origin && allowedOrigins.includes(origin)) headers.set("Access-Control-Allow-Origin", origin);
 
-	return new Response(JSON.stringify(linesFiltered), { status: 200, headers: headers });
+	return new Response(JSON.stringify(lines), { status: 200, headers: headers });
 };
 
 interface RawLine {
